467769f07f8db8277471e3e2c409799375b61a52
Maxim Faskhetdinov <nomaxismax@gmail.com>
2022-10-11 08:29:30 +0300

    initial commit

--- counterYP/ViewController.swift
+++ counterYP/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    private var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.text = "Значение счётчика: 0"
     }
 
-
+    @IBAction func buttonDidTap() {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+    }
+    
 }
 
